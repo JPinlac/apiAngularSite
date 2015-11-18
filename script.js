7babe4a0ac919131042afa456b4b4f6f7631e579
Fernando Jones <fernandoajones@gmail.com>
2015-11-17 20:44:18 -0500

    first push/pull

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ app.config(['$routeProvider', function($routeProvider){
             redirectTo: '/'
         });
 }]);
+
+app.factory('viewNy', function(){
+
+$http.get("http://api.nytimes.com/svc/semantic/v2/geocodes/query.json?name=Paris&api-key=c24f87cd1a530b710c48b2231f630d6c:11:73496093")
+    .success(function(response) {
+        $scope.articleData = response.results;
+    });
+});
+
+console.log(response.article);
+
+    var obj ={};
+            obj.setData = function(input1, input2){
+                this.input1 = input1; 
+                this.input2 = input2;
+
+
