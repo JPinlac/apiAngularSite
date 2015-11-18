fdedf385a153d97cd992fc69ef8c60e5f1098c5f
Jonathan <jonppinlac@gmail.com>
2015-11-17 21:31:10 -0500

    Added working arrawy

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ app.config(['$routeProvider', function($routeProvider){
 }]);
 
 
-app.controller('searchController', function($scope, getArticles, getGeoData){
+app.controller('searchController', function($scope, getArticles){
     $scope.articleList = getArticles.get("paris");
+});
+
+app.controller('savedController', function($scope, getGeoData){
     $scope.geoData = getGeoData.get("paris");
 });
 
@@ -25,10 +28,22 @@ app.factory('getArticles', function($http){
     service={};
     service.articleList=[];
 
+    function article(title,url){
+        this.title = title;
+        this.url = url;
+    }
+
     service.get = function(searchTerm){
         $http.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?fq='+searchTerm+'&api-key=1aac169c69edc3e8bc34be81972e67e2:10:73496041').success(function(response){
 
-            console.log(response);
+            console.log(response)
+            for(var i = 0; i < 10; i++){
+                var newArticle = new article(
+                    response.response.docs[i].headline.main,
+                    response.response.docs[i].web_url);
+                service.articleList.push(newArticle);
+            }
+            console.log(service.articleList);
             
         })
     };
@@ -47,21 +62,3 @@ app.factory('getGeoData', function($http){
     };
     return service2;
 });
-
-// app.factory('viewNy', function(){
-
-// $http.get("http://api.nytimes.com/svc/semantic/v2/geocodes/query.json?name=Paris&api-key=c24f87cd1a530b710c48b2231f630d6c:11:73496093")
-//     .success(function(response) {
-//         $scope.articleData = response.results;
-//     });
-// });
-
-// console.log(response.article);
-
-//     var obj ={};
-//             obj.setData = function(input1, input2){
-//                 this.input1 = input1; 
-//                 this.input2 = input2;
-
-
-
